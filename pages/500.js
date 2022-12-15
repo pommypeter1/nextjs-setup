17d95a63d1473dc60f521d16fb1aa71b2d45add5
Peter Jackson <peter.jackson@multichoice.com>
2022-12-15 13:51:07 +0200

    First commit

---
+++ pages/500.js
@@ -0,0 +1,13 @@
+// 500.js
+import Link from 'next/link'
+
+export default function FourOhFour() {
+  return <>
+    <h1>500 - Page Not Found</h1>
+    <Link href="/">
+      <a>
+        Go back home
+      </a>
+    </Link>
+  </>
+}
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Page(){
+    return (
+        <div>
+           <h1>Hello World</h1> 
+        </div>
+    );
+}
+export default Page;
